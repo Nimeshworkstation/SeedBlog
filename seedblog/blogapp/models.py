ed1b31630bdf28e31bdae9c97258f193439e70cf
Nimesh Ghimire <nimesh.ghimire99@hotmail.com>
2016-11-25 15:32:06 +0545

    i edited

--- seedblog/blogapp/models.py
+++ seedblog/blogapp/models.py
@@ -1,11 +1,14 @@
 from __future__ import unicode_literals
-
+from django.utils import timezone
 from django.db import models
 
 # Create your models here.
 class BlogPost(models.Model):
+	author = models.ForeignKey('auth.user', blank = True, null = True)
 	title =  models.CharField(max_length=200)
 	content = models.TextField()
 	def __str__(self):
 		return self.title
-		
+	created = models.DateTimeField(auto_now_add = True, null = True, blank = True)
+	updated = models.DateTimeField(auto_now_add = True, null = True, blank = True)
+	publish = models.BooleanField(default = False)
